e567dad35277413775d964e31e1fe9cddbd14ce6
yurchykyur <yurchyk.yur.s@gmail.com>
2023-09-11 23:45:03 +0300

    update toolbar, drawer - add isLogged
    add AuthNav, UserMenu

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,27 +2,30 @@ import Layout from 'layout';
 import Home from 'pages/Home';
 import Login from 'pages/Login';
 import Register from 'pages/Register';
-import {  Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
-       <Routes>
+      <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<h3><Home /></h3>} />
-          <Route path="/login" element={<h3><Login /></h3>} />
-          <Route path="/register" element={<h3><Register /></h3>} />
-          <Route path="/usefull-material" element={<h3>Usefull material</h3>} />
-          <Route path="/normative" element={<h3>Normative</h3>} />
+          <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+          <Route path="/dashboard" element={<h3>Dashboard</h3>} />
+
           <Route path="/freedom-finance" element={<h3>Freedom Finance</h3>} />
           <Route
             path="/interactive-brokers"
             element={<h3>Interactive Brokers</h3>}
           />
-         <Route path="/buy" element={<h3>Buy</h3>} />
+          <Route path="/buy" element={<h3>Buy</h3>} />
           <Route path="/sell" element={<h3>Sell</h3>} />
 
           <Route path="/generate-report" element={<h3>Generate a report</h3>} />
+          <Route path="/usefull-material" element={<h3>Usefull material</h3>} />
+          <Route path="/normative" element={<h3>Normative</h3>} />
 
           <Route path="*" element={<h3>NotFound </h3>} />
         </Route>
--- src/components/Drawer/ToolbarForDrawer.jsx
+++ src/components/Drawer/ToolbarForDrawer.jsx
@@ -1,47 +1,12 @@
 import { useState } from 'react';
 import Box from '@mui/material/Box';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import Avatar from '@mui/material/Avatar';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import LogoutIcon from '@mui/icons-material/Logout';
 import { Link as RouterLink } from 'react-router-dom';
+import UserMenu from './UserMenu';
+import AuthNav from './AuthNav';
+import { Link } from '@mui/material';
 
-
-const settings = ['Profile', 'Account', 'Dashboard', 'Choose theme'];
-
-function stringAvatar(name) {
-  return {
-    sx: {
-      bgcolor: stringToColor(name),
-    },
-    children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
-  };
-}
-
-function stringToColor(string) {
-  let hash = 0;
-  let i;
-
-  /* eslint-disable no-bitwise */
-  for (i = 0; i < string.length; i += 1) {
-    hash = string.charCodeAt(i) + ((hash << 5) - hash);
-  }
-
-  let color = '#';
-
-  for (i = 0; i < 3; i += 1) {
-    const value = (hash >> (i * 8)) & 0xff;
-    color += `00${value.toString(16)}`.slice(-2);
-  }
-  /* eslint-enable no-bitwise */
-
-  return color;
-}
-
-function ContentToolbarForDrawer() {
+function ContentToolbarForDrawer({ isLoggedIn }) {
   const [anchorElUser, setAnchorElUser] = useState(null);
 
   const handleOpenUserMenu = event => {
@@ -54,11 +19,12 @@ function ContentToolbarForDrawer() {
 
   return (
     <>
-      <Typography
+      <Link
         variant="h6"
         noWrap
         component={RouterLink}
-       to="/"
+        to="/"
+        underline="hover"
         sx={{
           mr: 10,
           display: { xs: 'none', md: 'flex' },
@@ -70,12 +36,13 @@ function ContentToolbarForDrawer() {
         }}
       >
         FIFO
-      </Typography>
-      <Typography
+      </Link>
+      <Link
         variant="h5"
         noWrap
         component={RouterLink}
-       to="/"
+        to="/"
+        underline="hover"
         sx={{
           mr: 2,
           display: { xs: 'flex', md: 'none' },
@@ -88,7 +55,7 @@ function ContentToolbarForDrawer() {
         }}
       >
         FIFO
-      </Typography>
+      </Link>
       <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
         <Typography
           variant="body1"
@@ -106,55 +73,16 @@ function ContentToolbarForDrawer() {
           First in - First out
         </Typography>
       </Box>
-      <Box sx={{}}>
-        <Typography
-          variant="body1"
-          noWrap
-          component="p"
-          sx={{
-            mr: 2,
-            display: { xs: 'none', md: 'flex' },
-            fontFamily: 'Roboto',
-            fontWeight: 500,
-            letterSpacing: '.15rem',
-            color: 'inherit',
-          }}
-        >
-          User name
-        </Typography>
-      </Box>
-      <Box sx={{ flexGrow: 0 }}>
-        <Tooltip title="Open settings">
-          <IconButton onClick={handleOpenUserMenu} sx={{ p: 0, mr: 2 }}>
-            <Avatar {...stringAvatar('Kent Dodds')} />
-          </IconButton>
-        </Tooltip>
-        <Menu
-          sx={{ mt: '45px' }}
-          id="menu-appbar"
-          anchorEl={anchorElUser}
-          anchorOrigin={{
-            vertical: 'top',
-            horizontal: 'right',
-          }}
-          keepMounted
-          transformOrigin={{
-            vertical: 'top',
-            horizontal: 'right',
-          }}
-          open={Boolean(anchorElUser)}
-          onClose={handleCloseUserMenu}
-        >
-          {settings.map(setting => (
-            <MenuItem key={setting} onClick={handleCloseUserMenu}>
-              <Typography textAlign="center">{setting}</Typography>
-            </MenuItem>
-          ))}
-        </Menu>
-        <IconButton aria-label="logout">
-          <LogoutIcon sx={{ color: '#f39fad' }} />
-        </IconButton>
-      </Box>
+
+      {isLoggedIn ? (
+        <UserMenu
+          valueAnchorElUser={anchorElUser}
+          onCloseUserMenu={handleCloseUserMenu}
+          onOpenUserMenu={handleOpenUserMenu}
+        />
+      ) : (
+        <AuthNav />
+      )}
     </>
   );
 }
