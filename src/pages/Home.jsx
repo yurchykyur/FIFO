8c0f977d1f16dd6c70bdb86230cc99b22b75371b
yurchykyur <yurchyk.yur.s@gmail.com>
2023-09-12 11:29:21 +0300

    update styling
    Conteiners, position, marginTop,

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,21 @@
-import { Container } from '@mui/material';
+import { Container, Box } from '@mui/material';
 import { Helmet } from 'react-helmet';
 
 export default function Home() {
   return (
-    <Container fixed>
+    <Container>
       <Helmet>
         <title>Home page</title>
       </Helmet>
-      <h1>
-        Phonebook welcome page{' '}
-        <span role="img" aria-label="Greeting icon">
-          💁‍♀️
-        </span>
-      </h1>
+
+      <Box sx={{ marginTop: { xs: 12, md: 20 } }}>
+        <h1>
+          Phonebook welcome page{' '}
+          <span role="img" aria-label="Greeting icon">
+            💁‍♀️
+          </span>
+        </h1>
+      </Box>
     </Container>
   );
 }
